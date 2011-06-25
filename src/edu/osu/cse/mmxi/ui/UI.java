bf1d58b73c46f57c97dff8fc3b1f48c14c655e4e
Alex Burkhart <saterus@gmail.com>
2011-06-25 03:28:53 -0400

    Introduced the concept of Modes to the UI.

--- src/edu/osu/cse/mmxi/ui/UI.java
+++ src/edu/osu/cse/mmxi/ui/UI.java
@@ -1,14 +1,31 @@
 package edu.osu.cse.mmxi.ui;
 
 public class UI {
-	public void error(String msg) {
-		System.err.println(msg);
-		System.exit(1);
-	}
-	public void warn(String msg) {
-		System.err.println(msg);
-	}
-	public void print(String msg) {
-		System.out.print(msg);
-	}
+
+    public enum UIMode {
+        QUIET, TRACE, STEP
+    };
+
+    private final UIMode mode;
+
+    public UI() {
+        this(UIMode.QUIET);
+    }
+
+    public UI(final UIMode mode) {
+        this.mode = mode;
+    }
+
+    public void error(final String msg) {
+        System.err.println(msg);
+        System.exit(1);
+    }
+
+    public void warn(final String msg) {
+        System.err.println(msg);
+    }
+
+    public void print(final String msg) {
+        System.out.print(msg);
+    }
 }
