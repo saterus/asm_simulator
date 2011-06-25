519d7b1542a660cdd2f35f1d47133607d7e15e62
Alex Burkhart <saterus@gmail.com>
2011-06-25 03:33:43 -0400

    Fixed the glue code and commented the Simulator.

--- src/edu/osu/cse/mmxi/Simulator.java
+++ src/edu/osu/cse/mmxi/Simulator.java
@@ -3,9 +3,8 @@ package edu.osu.cse.mmxi;
 import java.io.IOException;
 
 import edu.osu.cse.mmxi.loader.SimpleLoader;
-import edu.osu.cse.mmxi.loader.ParseException;
+import edu.osu.cse.mmxi.loader.parser.ParseException;
 import edu.osu.cse.mmxi.machine.Machine;
-import edu.osu.cse.mmxi.machine.memory.MemoryException;
 import edu.osu.cse.mmxi.ui.UI;
 
 public final class Simulator {
@@ -15,52 +14,52 @@ public final class Simulator {
     /**
      * The Simulator begins stepping the Machine's clock.
      * 
-     * The Simulator controls the Machine's clock by telling it when it is ready
-     * to advance to the next instruction and execute it.
+     * The Simulator controls the Machine's clock by telling it when it is ready to
+     * advance to the next instruction and execute it.
      * 
-     * If the Machine has halted or run beyond the maximum number of
-     * instructions, the clock stops.
+     * If the Machine has halted or run beyond the maximum number of instructions, the
+     * clock stops.
      * 
-     * During Step or Trace mode, we pass information from the Machine to the UI
-     * during each clock step.
+     * During Step or Trace mode, we pass information from the Machine to the UI during
+     * each clock step.
      */
-    public static void startClockLoop(Machine machine) {
+    public static void startClockLoop(final Machine machine) {
 
         while (machine.clockCount() < MAX_CLOCK_COUNT && !machine.hasHalted()) {
 
-            String instructionDetails = machine.stepClock();
+            final String instructionDetails = machine.stepClock();
             // TODO: if tracing/stepping, print instruction details and machine stats
             // if stepping, pause for user.
         }
     }
 
     // TODO Handle optional flag for [--step/--trace/--quiet] mode
-    public static void processArgs(String[] args) {
+    public static void processArgs(final String[] args) {
         if (args.length != 1) {
             System.err
                     .println("program requires exactly one argument: the file to be processed");
+            System.exit(1);
+            // TODO: exiting here might be harsher than we want.
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
 
         processArgs(args);
 
-        UI cli = new UI();
-        Machine machine = new Machine();
+        final UI cli = new UI();
+        final Machine machine = new Machine();
 
         try {
 
             SimpleLoader.load(args[0], machine);
 
-        } catch (MemoryException e) {
-            cli.error("error: attempted to write out of allowed memory");
-        } catch (ParseException e) {
+        } catch (final ParseException e) {
             cli.error(e.getMessage());
-        } catch (IOException e) {
+        } catch (final IOException e) {
             cli.error("I/O Error: " + e.getMessage());
         }
 
-        startClockLoop(machine);
+        // startClockLoop(machine);
     }
 }
