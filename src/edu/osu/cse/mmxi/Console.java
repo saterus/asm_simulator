feb7486f26100a7f0391dca2f7f3d4946f9ccf10
Mario Carneiro <di.gama@gmail.com>
2011-07-04 01:03:24 -0400

    Added some help text (not done yet)

--- src/edu/osu/cse/mmxi/Console.java
+++ src/edu/osu/cse/mmxi/Console.java
@@ -1,19 +1,14 @@
 package edu.osu.cse.mmxi;
 
-<<<<<<< HEAD
 import java.io.IOException;
 import java.util.Map;
 import java.util.TreeMap;
 
 import edu.osu.cse.mmxi.loader.SimpleLoader;
-import edu.osu.cse.mmxi.loader.parser.ParseException;
-=======
->>>>>>> 0f19d49beff374f09f64d97220057d337cd14348
 import edu.osu.cse.mmxi.machine.Machine;
 import edu.osu.cse.mmxi.machine.memory.MemoryUtilities;
 
 public class Console {
-<<<<<<< HEAD
     private final Machine             m;
     private int                       maxClock;
     private int                       memTrack;
@@ -45,37 +40,36 @@ public class Console {
             _break(words);
         else if ("clock".startsWith(words[0]))
             clock(words);
+        else if ("disasm".startsWith(words[0]))
+            disasm(words);
+        else if ("dump".startsWith(words[0]))
+            dump(words);
         else if ("edit".startsWith(words[0]))
             edit(words);
-        else if ("help".startsWith(words[0]))
+        else if ("exit".startsWith(words[0]) || "quit".startsWith(words[0]))
+            System.exit(0);
+        else if ("help".startsWith(words[0]) || words[0].equals("?"))
             help(words);
         else if ("load".startsWith(words[0]))
             load(words);
-        else if ("quit".startsWith(words[0]))
-            System.exit(0);
+        else if ("run".startsWith(words[0]))
+            run(words);
+        else if ("reg".startsWith(words[0]))
+            reg(words);
+        else if ("reset".startsWith(words[0]))
+            reset(words);
         else if ("step".startsWith(words[0]))
             step(false, words);
+        else if (words[0].length() > 3 && "track".startsWith(words[0]))
+            track(words);
         else if ("trace".startsWith(words[0]))
             step(true, words);
-        else if (words[0].equals("track"))
-            track(words);
         else if ("watch".startsWith(words[0]))
             watch(words);
-        else if (words[0].length() > 1) {
-            if ("disasm".startsWith(words[0]))
-                disasm(words);
-            else if ("dump".startsWith(words[0]))
-                dump(words);
-            else if ("reg".startsWith(words[0]))
-                reg(words);
-            else if ("reset".startsWith(words[0]))
-                reset(words);
-            else if ("run".startsWith(words[0]))
-                run(words);
-            else
-                m.ui.warn("Unknown command");
-        } else
-            m.ui.warn("Unknown command");
+        else {
+            m.ui.print("Unknown command\n");
+            help("help");
+        }
     }
 
     private void _break(final String... words) {
@@ -137,13 +131,13 @@ public class Console {
             maxClock = Simulator.MAX_CLOCK_COUNT = max;
         } else
             m.ui.print("Clock currently at " + (m.clockCount() - 1) + " instructions"
-                + (maxClock == 0 ? "." : " out of " + maxClock + "."));
+                + (maxClock == Integer.MAX_VALUE ? "." : " out of " + maxClock + "."));
     }
 
     private void disasm(final String... words) {
         int base = memTrack, len = 16;
         if (words.length > 1) {
-            if ((base = readAddr(words[1])) == -1 && !words[1].equalsIgnoreCase("pc")) {
+            if ((base = readAddr(words[1])) == -1) {
                 m.ui.print("Malformed address '" + words[1] + "'\n");
                 help("help", "disasm");
                 return;
@@ -186,7 +180,7 @@ public class Console {
     private void dump(final String... words) {
         int base = memTrack, len = 8;
         if (words.length > 1) {
-            if ((base = readAddr(words[1])) == -1 && !words[1].equalsIgnoreCase("pc")) {
+            if ((base = readAddr(words[1])) == -1) {
                 m.ui.print("Malformed address '" + words[1] + "'\n");
                 help("help", "dump");
                 return;
@@ -250,6 +244,113 @@ public class Console {
     }
 
     private void help(final String... words) {
+        final String help = " Syntax: help [keyword]\n"
+            + " Mnemonics: ? h he hel help\n\n"
+            + " The help command provides information on specific commands and features.\n"
+            + " If used without any arguments, help will display a short list of commands\n"
+            + " available in the simulator.\n\n"
+            + " You can get additional help on specific commands by typing help followed\n"
+            + " by the command or keyword you want more information on.\n\n COMMANDS:\n"
+            + "    break           clock           disasm          dump\n"
+            + "    edit            help            load            quit\n"
+            + "    reg             reset           run             step\n"
+            + "    trace           track           watch";
+        if (words.length < 2 || words[1].length() == 0)
+            m.ui.print(help);
+        else if ("break".startsWith(words[1]))
+            m.ui.print(" Syntax: break [-d] <address>\n         break [-D]\n"
+                + " Mnemonics: b br bre brea break\n\n"
+                + " Breakpoints allow you to stop execution of the program when certain\n"
+                + " addresses are reached. The breakpoint facility functions during trace,\n"
+                + " step, and execution modes.\n\n"
+                + " Breakpoints can be listed, added, or removed.\n\n"
+                + "   > break           Lists the currently set breakpoints\n"
+                + "   > break -D        Deletes all breakpoints\n"
+                + "   > break x3021     Sets breakpoint at address 0x3021\n"
+                + "   > break -d x3021  Removes the breakpoint\n\n"
+                + " A message will be displayed to notify you if you try to set a breakpoint\n"
+                + " that already exists, or try to delete a breakpoint that does not exist.");
+        else if ("clock".startsWith(words[1]))
+            m.ui.print(" Syntax: clock [<max>]\n" + " Mnemonics: c cl clo cloc clock\n\n");
+        else if ("disasm".startsWith(words[1]))
+            m.ui.print(" Syntax: disasm [<address> [halt|ret|<length>]]\n"
+                + " Mnemonics: d di dis disa disas disasm\n\n"
+                + " The disasm command provides a disassembled interpretation of the words\n"
+                + " located at the specified address. The optional second argument allows\n"
+                + " the choice of having a set number of instructions disassembled, or to\n"
+                + " continue disassembling until a TRAP HALT or RET command is encountered.\n\n"
+                + " The command will provide assembly mnemonic listings for 16 consecutive\n"
+                + " words (by default). The first column of the listing specifies the\n"
+                + " address of the word being disassembled, the second column contains the\n"
+                + " hexadecimal data at that word, while the remaining columns provide the\n"
+                + " disassembled instructions.\n\n"
+                + "   > disasm           Disassembles starting at the current memory track\n"
+                + "                      (see 'help track' for details)\n"
+                + "   > disasm pc halt   Disassembles starting at PC, and continuing until\n"
+                + "                      TRAP HALT is found\n"
+                + "   > disasm x3000     Disassembles 16 instructions starting at 0x3000\n\n"
+                + "   > disasm x1000 20  Disassembles 20 instructions starting at 0x1000\n"
+                + "   > disasm r0 ret    Disassembles starting at the address in register\n"
+                + "                      0, and continuing until RET is found");
+        else if ("dump".startsWith(words[1]))
+            m.ui.print(" Syntax: dump [<address> [<length>]]\n"
+                + " Mnemonics: du dum dump\n\n"
+                + " The dump Command allows the user to inspect memory.\n\n"
+                + " Without arguments, the dump command will display 128 words of memory\n"
+                + " starting from the memory track (see 'help track' for details).\n\n"
+                + " Optionally, an address can be specified to the dump command which will\n"
+                + " result in the dump starting from the specified address. An optional\n"
+                + " length parameter allows the number of lines of data to be specified,\n"
+                + " defaulting to 8 lines.\n\n"
+                + "   > dump        Display memory starting at the memory track\n"
+                + "   > dump x1000  Display memory starting at address 0x1000\n"
+                + "   > dump pc 20  Display 20 lines (320 words) of memory\n"
+                + "                 starting at the PC");
+        else if ("edit".startsWith(words[1]))
+            m.ui.print(" Syntax: edit <address> <value>\n"
+                + " Mnemonics: e ed edi edit\n\n"
+                + " The edit command allows users to modify memory locations. Either of the\n"
+                + " fields 'address' or 'value' may be register names in order to change\n"
+                + " memory locations pointed to by those registers.\n\n"
+                + "   > edit x1000 x001C  Replace the word at 0x1000 with the value 0x001C\n"
+                + "   > edit r0 pc        Set the memory loaction pointed to by register 0\n"
+                + "                       to the current value of the PC");
+        else if ("exit".startsWith(words[1]) || "quit".startsWith(words[1]))
+            m.ui.print(" Syntax: quit\n" + " Mnemonics: ex exi exit q qu qui quit\n\n"
+                + " Exit from the simulator program.");
+        else if ("load".startsWith(words[1]))
+            m.ui.print(" Syntax: load <file>\n"
+                + " Mnemonics: l lo loa load\n\n"
+                + " The load command loads a compiled assembly language program from the\n"
+                + " disk. The program will be in its initial state after loading and will\n"
+                + " need to be started via the run command afterwards.\n\n"
+                + "   > load test.out       loads 'test.out' from current directory\n"
+                + "   > load /usr/bin/test  loads 'test' from /usr/bin/\n\n"
+                + " The file must be in MMXI file format. Otherwise, any parsing errors\n"
+                + " will be displayed on-screen, with the machine in an indeterminate\n"
+                + " state afterwards (another load command will reset the machine).");
+        else if ("run".startsWith(words[1]))
+            m.ui.print(" Syntax: run\n" + " Mnemonics: r ru run\n\n" + " TODO: Text here");
+        else if ("reg".startsWith(words[1]))
+            m.ui.print(" Syntax: reg [pc|flags|r<num> [<value>]]\n"
+                + " Mnemonics: re reg\n\n" + " TODO: Text here");
+        else if ("reset".startsWith(words[1]))
+            m.ui.print(" Syntax: reset [-l] [<value>|rand]\n"
+                + " Mnemonics: res rese reset\n\n" + " TODO: Text here");
+        else if ("step".startsWith(words[1]))
+            m.ui.print(" Syntax: step [<steps>]\n" + " Mnemonics: s st ste step\n\n"
+                + " TODO: Text here");
+        else if (words[1].length() > 3 && "track".startsWith(words[1]))
+            m.ui.print(" Syntax: track pc|<address>\n" + " Mnemonics: trac track\n\n"
+                + " TODO: Text here");
+        else if ("trace".startsWith(words[1]))
+            m.ui.print(" Syntax: trace [<steps>]\n" + " Mnemonics: t tr tra trace\n\n"
+                + " TODO: Text here");
+        else if ("watch".startsWith(words[1]))
+            m.ui.print(" Syntax: watch [-d] <address>\n         watch [-D]\n"
+                + " Mnemonics: w wa wat watc watch\n\n" + " TODO: Text here");
+        else
+            m.ui.print(help);
 
     }
 
@@ -262,12 +363,10 @@ public class Console {
         m.ui.print("Loading file: " + file + "\n");
         try {
             SimpleLoader.load(file, m);
-        } catch (final ParseException e) {
-            m.ui.error(e.getMessage());
+            printInstruction();
         } catch (final IOException e) {
-            m.ui.error("I/O Error: " + e.getMessage());
+            m.ui.print("I/O Error: " + e.getMessage());
         }
-        printInstruction();
     }
 
     private void reg(final String... words) {
@@ -347,12 +446,12 @@ public class Console {
                 steps = Integer.parseInt(words[1]);
                 if (steps < 0) {
                     m.ui.print("'steps' cannot be negative.");
-                    help("help", "trace");
+                    help("help", trace ? "trace" : "step");
                     return;
                 }
             } catch (final NumberFormatException e) {
                 m.ui.print("'steps' must be a number.");
-                help("help", "trace");
+                help("help", trace ? "trace" : "step");
                 return;
             }
         runMachine(steps);
@@ -362,17 +461,17 @@ public class Console {
     }
 
     private void track(final String... words) {
-        if (words.length < 1) {
-            help("help", "load");
+        if (words.length < 2) {
+            help("help", "track");
             return;
         }
-        if (words[0].equalsIgnoreCase("pc")) {
+        if (words[1].equalsIgnoreCase("pc")) {
             m.ui.print("Tracking Program Counter.");
             memTrack = -1;
         } else {
             if (readAddr(words[1]) == -1) {
                 m.ui.print("Invalid tracking address.");
-                help("help", "trace");
+                help("help", "track");
                 return;
             } else
                 memTrack = readAddr(words[1]);
@@ -550,95 +649,4 @@ public class Console {
             }
         }
     }
-=======
-
-    public Console(final Machine m, final String file) {
-        if (file == null) {
-            m.ui.warn("No files given!");
-            System.exit(1);
-        }
-
-        int memTrack = -1;
-        while (!m.hasHalted()) {
-            short mem;
-            if (memTrack == -1)
-                mem = (short) ((m.getPCRegister().getValue() & 0xfff8) - 8);
-            else
-                mem = (short) memTrack;
-            m.ui.print("\n                           ");
-            for (int j = 0; j < 8; j++)
-                m.ui.print(" --" + (mem + j & 7) + "-");
-            m.ui.print("\n");
-            for (int i = 0; i < 4; i++) {
-                m.ui.print("R" + 2 * i + ": ");
-                m.ui.print(MemoryUtilities.uShortToHex(m.getRegister(2 * i).getValue())
-                    + "  ");
-                m.ui.print("R" + (2 * i + 1) + ": ");
-                m.ui.print(MemoryUtilities.uShortToHex(m.getRegister(2 * i + 1)
-                    .getValue()) + "   ");
-                m.ui.print(MemoryUtilities.uShortToHex((short) (mem + 8 * i)) + " | ");
-                for (int j = 0; j < 8; j++)
-                    m.ui.print(MemoryUtilities.uShortToHex(m.getMemory((short) (mem + 8
-                        * i + j)))
-                        + " ");
-                m.ui.print("\n");
-            }
-            m.ui.print("\n          PC: "
-                + MemoryUtilities.uShortToHex(m.getPCRegister().getValue()) + "  ");
-            m.ui.print((m.getFlags().getN() ? "n" : "-")
-                + (m.getFlags().getZ() ? "z" : "-") + (m.getFlags().getP() ? "p" : "-")
-                + "  ");
-            m.ui.print(MemoryUtilities.uShortToHex(m.getMemory(m.getPCRegister()
-                .getValue())) + ": ");
-            m.ui.print(m.alu.readInstructionAt(m.getPCRegister().getValue()) + "\n\n");
-            while (true) {
-                String s = m.ui.prompt("Press ENTER to step, "
-                    + "or a hex address or 'pc' to track memory:\n> ");
-                if (s.length() != 0) {
-                    memTrack = -2;
-                    while (true) {
-                        try {
-                            if (s.equalsIgnoreCase("pc"))
-                                memTrack = -1;
-                            else {
-                                memTrack = Integer.parseInt(s, 16);
-                                if ((memTrack & 0xffff0000) != 0)
-                                    memTrack = -2;
-                            }
-                        } catch (final NumberFormatException e) {
-                        }
-                        if (memTrack == -2)
-                            s = m.ui.prompt("Invalid hex or "
-                                + "number out of range.\n> ");
-                        else
-                            break;
-                    }
-                    if (memTrack == -1)
-                        mem = (short) ((m.getPCRegister().getValue() & 0xfff8) - 8);
-                    else
-                        mem = (short) memTrack;
-                    m.ui.print("\n      ");
-                    for (int j = 0; j < 16; j++)
-                        m.ui.print(" --"
-                            + Integer.toHexString(mem + j & 15).toUpperCase() + "-");
-                    m.ui.print("\n");
-                    for (int i = 0; i < 8; i++) {
-                        m.ui.print(MemoryUtilities.uShortToHex((short) (mem + 16 * i))
-                            + " | ");
-                        for (int j = 0; j < 16; j++)
-                            m.ui.print(MemoryUtilities.uShortToHex(m
-                                .getMemory((short) (mem + 16 * i + j))) + " ");
-                        m.ui.print("\n");
-                    }
-                    m.ui.print("\n");
-                } else
-                    break;
-            }
-
-            m.stepClock();
-        }
-        m.ui.print("Machine halted after " + (m.clockCount() - 1) + " steps.");
-    }
-
->>>>>>> 0f19d49beff374f09f64d97220057d337cd14348
 }
