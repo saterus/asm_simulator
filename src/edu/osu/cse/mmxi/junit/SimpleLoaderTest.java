b28cd9b23efcd8b9d775bbbcf3561a23ab4fe46a
Will Smelser <willsmelser@gmail.com>
2011-07-04 20:14:51 -0400

    Review SimpleLoaderTest started adding some more testing.

--- src/edu/osu/cse/mmxi/junit/SimpleLoaderTest.java
+++ src/edu/osu/cse/mmxi/junit/SimpleLoaderTest.java
@@ -11,18 +11,22 @@ import edu.osu.cse.mmxi.loader.SimpleLoader;
 import edu.osu.cse.mmxi.machine.Machine;
 
 public class SimpleLoaderTest {
-    Machine m = null;
+    String  dir = "src/edu/osu/cse/mmxi/junit";
+    Machine m   = null;
 
     @Before
     public void init() {
         m = new Machine();
+
     }
 
+    // test file exists, but not readable, using a directory
     @Test(expected = IOException.class)
     public void testDirectoryRead() throws IOException {
         SimpleLoader.load("src", m);
     }
 
+    // testing that a file doesnt exist error
     @Test(expected = IOException.class)
     public void testNonexistentRead() throws IOException {
         SimpleLoader.load("garbage", m);
@@ -30,11 +34,31 @@ public class SimpleLoaderTest {
 
     @Test
     public void testRead() throws IOException {
-        SimpleLoader.load("sample2.txt", m);
+        SimpleLoader.load(dir + "/jSample1.txt", m);
+
+        // array of memory locations to verify against
         final int[] dat = { 0x0004, 0x22B0, 0xE0B7, 0xF022, 0x127F, 0x02B3, 0xF025,
                 0x0068, 0x0069, 0x0020, 0x0000 };
+
+        // test memory was loaded properly
         for (int i = 0; i < dat.length; i++)
             assertEquals(m.getMemory((short) (0x30B0 + i)), (short) dat[i]);
+
         assertEquals(m.getPCRegister().getValue(), (short) 0x30B1);
     }
+
+    // test memory out of bounds error
+
+    // test exec out of bounds error
+
+    // test error messages
+
+    // test parser indirectly
+    // bad header
+    // bad main
+    // bad exec
+    // does error tracking work?
+    // good header, h vs. H
+    // good main, good in = good out
+    // good exec
 }
